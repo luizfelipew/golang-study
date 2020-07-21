09f94ed7c55dd0fe9971271387edf037b68439ae
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-07-20 22:36:53 -0300

    controle de fluxo utilizando if

--- script/first.go
+++ script/first.go
@@ -22,4 +22,14 @@ func main() {
 
 	fmt.Println("O endereco da variavel comando eh", &comando)
 	fmt.Println("O comando escolhido foi:", comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa")
+	} else {
+		fmt.Println("Näo conheço esse comando")
+	}
 }
