50c392e6290ddff463ee20aaaa12aeae2bae8937
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-07-24 12:27:36 -0300

    monitorando os nossos sites randomicos

--- script/first.go
+++ script/first.go
@@ -9,20 +9,23 @@ import (
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Näo conheço esse comando")
-		os.Exit(-1)
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Näo conheço esse comando")
+			os.Exit(-1)
+		}
 	}
 
 }
@@ -55,7 +58,12 @@ func leComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "http://www.alura.com.br"
+	site := "https://random-status-code.herokuapp.com/"
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!!!")
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
+	}
 }
