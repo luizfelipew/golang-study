b8a17064fce80f93de20d590560815a3798a1621
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-07-31 14:47:03 -0300

    add structs

--- banco/main.go
+++ banco/main.go
@@ -2,18 +2,14 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
 func main() {
-	var titular string = "Luiz Felipe"
-	var numeroAgencia int = 589
-	var numeroConta int = 123456
-	var saldo float64 = 125.50
 
-	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
-
-	var titular2 string = "Pati"
-	var numeroAgencia2 int = 5810
-	var numeroConta2 int = 123456789
-	var saldo2 float64 = 300.50
-
-	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+	fmt.Println(ContaCorrente{})
 }
