c07b52495ca6289e3624956dc5c4da190ef1d90c
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-07-31 13:18:39 -0300

    variaveis e tipos

---
+++ banco/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Luiz Felipe"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 125.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+	var titular2 string = "Pati"
+	var numeroAgencia2 int = 5810
+	var numeroConta2 int = 123456789
+	var saldo2 float64 = 300.50
+
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+}
