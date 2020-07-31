fc4a5551cba418c1530b5feffd036e59b0f1aa22
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-07-31 16:04:02 -0300

    new e ponteiros

--- banco/main.go
+++ banco/main.go
@@ -10,6 +10,18 @@ type ContaCorrente struct {
 }
 
 func main() {
+	contaDoLuiz := ContaCorrente{titular: "Luiz Felipe", numeroAgencia: 589,
+		numeroConta: 123456, saldo: 125.50}
 
-	fmt.Println(ContaCorrente{})
+	contaDaPati := ContaCorrente{"Pati", 1000, 100000, 300.50}
+
+	fmt.Println(contaDoLuiz)
+	fmt.Println(contaDaPati)
+
+	var contaDaCris *ContaCorrente
+	contaDaCris = new(ContaCorrente)
+	contaDaCris.titular = "Cris"
+	contaDaCris.saldo = 500
+
+	fmt.Println(*contaDaCris)
 }
