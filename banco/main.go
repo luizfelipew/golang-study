f25c4469d7bb5d502e81a46a0abd5d9fc67c2957
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-08-03 10:18:58 -0300

    criando metodo sacar

--- banco/main.go
+++ banco/main.go
@@ -9,19 +9,24 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
-	contaDoLuiz := ContaCorrente{titular: "Luiz Felipe", numeroAgencia: 589,
-		numeroConta: 123456, saldo: 125.50}
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	contaDaPati := ContaCorrente{"Pati", 1000, 100000, 300.50}
+	fmt.Println(contaDaSilvia.saldo)
 
-	fmt.Println(contaDoLuiz)
-	fmt.Println(contaDaPati)
+	fmt.Println(contaDaSilvia.Sacar(300))
 
-	var contaDaCris *ContaCorrente
-	contaDaCris = new(ContaCorrente)
-	contaDaCris.titular = "Cris"
-	contaDaCris.saldo = 500
-
-	fmt.Println(*contaDaCris)
+	fmt.Println(contaDaSilvia.saldo)
 }
