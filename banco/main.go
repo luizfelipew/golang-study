d31ee870bdb1a1ca85bd4296458d406d3bde8fdd
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-08-04 18:38:12 -0300

    adicionando pacote e visibilidade

--- banco/main.go
+++ banco/main.go
@@ -1,46 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Deposito realizado com sucesso", c.saldo
-	} else {
-		return "Valor do deposito menor que zero", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
-		c.saldo -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-		return true
-	} else {
-		return false
-	}
-}
+	"./contas"
+)
 
 func main() {
-	contaDaPati := ContaCorrente{titular: "Pati", saldo: 300}
-	contaDoLuiz := ContaCorrente{titular: "Luiz", saldo: 100}
+	contaDaPati := contas.ContaCorrente{Titular: "Pati", Saldo: 300}
+	contaDoLuiz := contas.ContaCorrente{Titular: "Luiz", Saldo: 100}
 
 	status := contaDoLuiz.Transferir(200, &contaDaPati)
 
