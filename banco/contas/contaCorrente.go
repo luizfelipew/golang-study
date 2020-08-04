b053f3ba8fb28573e5b9e642db569ba16e24da61
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-08-04 18:49:29 -0300

    add composição de structs

--- banco/contas/contaCorrente.go
+++ banco/contas/contaCorrente.go
@@ -1,7 +1,9 @@
 package contas
 
+import "../clientes"
+
 type ContaCorrente struct {
-	Titular       string
+	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
 	Saldo         float64
