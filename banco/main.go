82d3494308f84dc9e85264a565c3d253c5eef950
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-08-05 19:31:02 -0300

    tipos aninhados

--- banco/main.go
+++ banco/main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"fmt"
 
+	"./clientes"
 	"./contas"
 )
 
 func main() {
-	contaDaPati := contas.ContaCorrente{Titular: "Pati", Saldo: 300}
-	contaDoLuiz := contas.ContaCorrente{Titular: "Luiz", Saldo: 100}
+	// contaDaPati := contas.ContaCorrente{Titular: "Pati", Saldo: 300}
+	// contaDoLuiz := contas.ContaCorrente{Titular: "Luiz", Saldo: 100}
 
-	status := contaDoLuiz.Transferir(200, &contaDaPati)
+	// status := contaDoLuiz.Transferir(200, &contaDaPati)
 
-	fmt.Println(status)
+	// fmt.Println(status)
+
+	// contaDaPati := contas.ContaCorrente{Titular: clientes.Titular{
+	// 	Nome:      "Pati",
+	// 	CPF:       "123.111.123.12",
+	// 	Profissao: "Desenvolvedor GO"},
+	// 	NumeroAgencia: 123, NumeroConta: 123456, Saldo: 100}
+
+	clientePati := clientes.Titular{"Pati", "123.111.123.12", "Desenvolvedor GO"}
+	contaDaPati := contas.ContaCorrente{clientePati, 123, 123456, 100}
+
 	fmt.Println(contaDaPati)
-	fmt.Println(contaDoLuiz)
+
 }
